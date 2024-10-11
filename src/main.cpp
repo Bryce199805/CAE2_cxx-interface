5e38dc2fc86bd4a730632648b2e654f8ac8ef58a
Bryce199805 <bryce199805@gmail.com>
2024-10-11 20:21:22 +0800

    调整测试代码

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,14 @@
 #include "DamengDB.h"
 
 int main() {
+
     DamengDB obj("../config.yaml");
     std::string query_sql = "select * from TEST.USERS";
-    obj.Query(query_sql);
-    std::string insert_sql = "insert into TEST.USERS values (5, 'aaa', 12344, 708, 'aaa', 'aaa', 1)";
-    obj.Insert(insert_sql);
+    // std::string query_sql = "select * FROM TEST.T_JSON_OBJECT WHERE JSON_VALUE(c1, '$.name') = 'ÕÅÈý'";
 
+    obj.Query(query_sql);
+    // std::string insert_sql = "insert into TEST.USERS values (5, 'aaa', 12344, 708, 'aaa', 'aaa', 1)";
+    // std::string insert_sql = "select * FROM TEST.T_JSON_OBJECT WHERE JSON_VALUE(c1, '$.ÐÕÃû') = '123'";
+    // obj.Insert(insert_sql);
+    return 0;
 }
