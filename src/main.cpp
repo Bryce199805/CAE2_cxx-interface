a254ef9da31fb9358e5385b294332fe0a156cfe2
Bryce199805 <bryce199805@gmail.com>
2024-10-13 19:14:20 +0800

    更新测试样例

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,20 @@
 int main() {
 
     DamengDB obj("../config.yaml");
-    std::string query_sql = "select * from TEST.USERS";
+    std::string query_sql = "select * from BASIC_SHIP_INFORMATION_DB.SHIP_DATA_INFO";
     // std::string query_sql = "select * FROM TEST.T_JSON_OBJECT WHERE JSON_VALUE(c1, '$.name') = '张三'";
+    // 查询测试
+    std::vector<std::vector<std::string>> result1;
+    std::vector<std::vector<std::variant<std::string, int, double>>> result2;
+    std::vector<int> col_types;
+    if(obj.Query(query_sql, result1)) {
+        obj.printResult(result1);
+    }
 
-    obj.Query(query_sql);
+    if(obj.Query(query_sql, result2, col_types)) {
+        obj.printResult(result2, col_types);
+    }
+
     // std::string insert_sql = "insert into TEST.USERS values (5, 'aaa', 12344, 708, 'aaa', 'aaa', 1)";
     // std::string insert_sql = "select * FROM TEST.T_JSON_OBJECT WHERE JSON_VALUE(c1, '$.姓名') = '123'";
     // obj.Insert(insert_sql);
