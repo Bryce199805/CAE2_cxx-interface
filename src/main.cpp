1428d2a776aed39b38239638b3de1492ffa7b3d2
chenyu-T <954972374@qq.com>
2024-12-05 15:47:10 +0800

    修正错误

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,15 @@
 int main() {
     CAE obj("../config.yaml", true);
 
-    // 查询测试
-    std::string query_sql = "select * from BASIC_SHIP_INFORMATION_DB.SHIP_DATA_INFO where ship_type = '油船'";
-    std::vector<std::vector<DBVariant> > result2;
-    std::vector<int> col_types;
-
-    if (obj.Query(query_sql, result2, col_types)) {
-        // 输出打印测试
-        obj.printResult(result2, col_types);
-    }
+    // // 查询测试
+    // std::string query_sql = "select * from BASIC_SHIP_INFORMATION_DB.SHIP_DATA_INFO where ship_type = '油船'";
+    // std::vector<std::vector<DBVariant> > result2;
+    // std::vector<int> col_types;
+    //
+    // if (obj.Query(query_sql, result2, col_types)) {
+    //     // 输出打印测试
+    //     obj.printResult(result2, col_types);
+    // }
 
     // 插入测试
     // std::string insert_sql =
@@ -42,35 +42,31 @@ int main() {
 
     //文件操作测试
 
-    //上传文件 指定路径，带文件名 hull_parameter_info
-    // if (obj.UploadFile("hull_model_and_information_db", "hull_parameter_info", "SampleShip_JBC0000", "TRANSVERSE_AREA_CURVE",
-    //                    "../doc/鲨鱼.png")) {
+    // //上传文件 指定路径，带文件名 hull_parameter_info
+    // if (obj.UploadFile("hull_model_and_information_db", "hull_parameter_info", "SampleShip_JBC0000", "TRANSVERSE_AREA_CURVE", "../doc/鲨鱼.png")) {
     //     std::cout << "Upload file done." << std::endl;
     // }
-    // if (obj.UploadFile("hull_model_and_information_db", "hull_parameter_info", "SampleShip_JBC0000", "HULL_3D_MODEL",
-    //                        "../doc/sharks.png")) {
+    // if (obj.UploadFile("hull_model_and_information_db", "hull_parameter_info", "SampleShip_JBC0000", "HULL_3D_MODEL","../doc/sharks.png")) {
     //     std::cout << "Upload file done." << std::endl;
     // }
     // // 下载文件 指定路径，不带文件名
     //
-    // if (obj.GetFile("hull_model_and_information_db", "HULL_PARAMETER_INFO", "SampleShip_JBC0000", "TRANSVERSE_AREA_CURVE",
-    //                 ".")) {
+    // if (obj.GetFile("hull_model_and_information_db", "HULL_PARAMETER_INFO", "SampleShip_JBC0000", "TRANSVERSE_AREA_CURVE",".")) {
     //     std::cout << "Download file done." << std::endl;
     // }
-
+    //
     // //下载字符流
     // std::vector<unsigned char> object_data;
-    // if (obj.GetFile("HULL_MODEL_AND_INFORMATION_DB", "hull_parameter_info", "M7081004", "HULL_3D_MODEL",
-    //                 object_data)) {
+    // if (obj.GetFile("HULL_MODEL_AND_INFORMATION_DB", "hull_parameter_info", "M7081004", "HULL_3D_MODEL",object_data)) {
     //     std::cout << "Download data stream done." << std::endl;
     //     std::cout << "object_data size: " << object_data.size() << std::endl;
     // }
     //
-    // 删除文件
+    // // 删除文件
     // if (obj.DeleteFile("HULL_MODEL_AND_INFORMATION_DB", "hull_parameter_info", "M7081004", "HULL_3D_MODEL")) {
     //     std::cout << "Delete file done." << std::endl;
     // }
-
+    //
     // //删除记录
     // if (obj.DeleteRecord("hull_model_and_information_db", "HULL_PARAMETER_INFO", "M7081004")) {
     //     std::cout << "Delete record done." << std::endl;
